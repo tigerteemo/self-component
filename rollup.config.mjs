28735e9d216e9e029132265788eef853d640af50
tigerteemo <972571862@qq.com>
2023-06-14 17:21:22 +0800

    optimizing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from 'rollup-plugin-postcss';
 import dts from "rollup-plugin-dts";
 
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 // const packageJson = require("./package.json");
 
 export default [
@@ -22,11 +24,15 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       // for css
       postcss(),
+
+      terser(),
     ],
   },
   {
